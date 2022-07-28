900b7767012be0017576945bdbfed0f136ccaa68
Michael Elliott <m.elliott92@yahoo.com>
2022-07-28 07:29:52 -0700

    perf!: Add numba jit compilation to noise funcs

--- gui.py
+++ gui.py
@@ -2,20 +2,20 @@ from perlin import perlin, noise2D
 import pygame
 
 pygame.init()
+clock = pygame.time.Clock()
 
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Perlin Noise Visualizer")
 
 octaves = 1
+seed = 1
 
 def drawNoise():
     for i in range(800):
         for j in range(600):
-            value = int(noise2D(i,j,octaves) * 255)
+            value = int(noise2D(i+seed,j+seed,octaves) * 255)
             window.set_at((i, j), (value, value, value))
     pygame.display.update()
-
-drawNoise()
 
 while True:
     for e in pygame.event.get():
@@ -25,8 +25,13 @@ while True:
         elif e.type == pygame.KEYDOWN:
             if e.key == pygame.K_UP:
                 octaves += 1
-                drawNoise()
             elif e.key == pygame.K_DOWN:
-                octaves -= 1
-                drawNoise()
-            pygame.display.update()
+                if octaves > 1:
+                    octaves -= 1
+    if seed >= 1000:
+        seed = 1
+    else:
+        seed += 1
+    drawNoise()
+    pygame.display.update()
+    clock.tick(60)
--- perlin.py
+++ perlin.py
@@ -1,9 +1,11 @@
 import math
 import numpy as np
+import time
+from numba import njit
 
 # ----------------- Permutation Table and Possible Gradients ----------------- #
 
-p = [151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 
+p = np.array([151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 
         103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 
         26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 
         87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 
@@ -17,22 +19,23 @@ p = [151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140,
         104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 
         241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 
         157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 
-        93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180 ]
+        93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180 ])
 
-# The eight possible gradient vectors
-gradients = [   [   1,  1], 
-                [   1,  0], 
-                [   1, -1], 
-                [   0,  1], 
-                [   0, -1], 
-                [  -1,  1], 
-                [  -1,  0], 
-                [  -1, -1]      ]
+gradients = np.array([
+    [   1,  1],
+    [   1,  0],
+    [   1, -1],
+    [   0,  1],
+    [   0, -1],
+    [  -1,  1],
+    [  -1,  0],
+    [  -1, -1]   ])
 
 # ============================================================================ #
 #                                Noise Functions                               #
 # ============================================================================ #
 """Generate noise value between 0 and 1, with the given number of frequency octaves"""
+@njit
 def noise2D(x: float, y: float, octaves: int) -> float:
     total = 0
     maxVal = 0
@@ -47,6 +50,7 @@ def noise2D(x: float, y: float, octaves: int) -> float:
     return total / maxVal
 
 """Generate noise value between 0 and 1 for any given coordinate"""
+@njit
 def perlin(x: float, y: float) -> float:
 
     # Get base grid integer coordinate
@@ -87,18 +91,40 @@ def perlin(x: float, y: float) -> float:
 # ============================================================================ #
 
 """Dot product of two vectors"""
+@njit
 def dot(x1: float, y1: float, x2: float, y2: float) -> float:
     return x1 * x2 + y1 * y2
 
 """The point on a smooth curve ("Smootherstep" Equation)"""
+@njit
 def smooth(t: float) -> float:
     return t*t*t*(t*(t*6.0-15.0)+10.0)
 
 """Linear interpolate two values using weight w"""
+@njit
 def interpolate(a: float, b: float, w: float) -> float:
     return a + smooth(w) * (b - a)
 
 """Grab a pseudorandom gradient value for any given integer coordinate"""
+@njit
 def grad(x: int) -> int:
     gIndex = x % 8
-    return gradients[gIndex]
+    return gradients[gIndex]      
+
+# ============================================================================ #
+#                                    Testing                                   #
+# ============================================================================ #
+
+# --------------------------------- Benchmark -------------------------------- #
+# elapsed = []
+# for s in range(10):
+#     start = time.time()
+#     for i in range(1000):
+#         for j in range(1000):
+#             perlin(i, j)
+#     end = time.time()
+#     elapsed.append(float(end - start))
+# result = np.average(elapsed)
+# print(f"Simple Perlin Noise:")
+# print(f"-------------------------")
+# print(f"Average elapsed time: {result}s")
